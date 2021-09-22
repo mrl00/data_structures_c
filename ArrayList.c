52fdb26cb8f995cdd9010fd4680a0b9b25131bd3
mrl00 <m2l_0@outlook.com>
2021-09-22 18:54:52 -0300

    add qsort example

--- ArrayList.c
+++ ArrayList.c
@@ -1,6 +1,6 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 typedef struct {
   void**  values;
@@ -14,7 +14,8 @@ ArrList* init_list(size_t capacity, int (*compare)(const void*, const void*)) {
   ArrList *l = (ArrList*) calloc(1, sizeof(ArrList));
   l->capacity = capacity;
   l->len = 0;
-  l->values = (void**) calloc(capacity, sizeof(void*));
+  l->values = (void**) calloc(capacity + 1, sizeof(void*)); 
+  l->values[capacity] = NULL;
   l->compare = compare;
   l->minimum = l->maximum = NULL;
   return l;
@@ -40,8 +41,38 @@ void destroy_arrlist(ArrList *list, void (*destroy_value)(void* value)) {
   free(list);
 }
 
+void walk(ArrList *l, void (*action)(void*)) {
+  for(void** i = l->values; *i != NULL; ++i)
+    action((*i));
+}
+
 bool is_empty(ArrList *list) { return list->len == 0;}
 
+typedef struct {
+  int a;
+} Test;
+
+int compare_test(const void* a, const void* b) {
+  Test *x = (Test*) a;
+  Test *y = (Test*) b;
+  return (y->a - x->a);
+}
+
+void show_test(void *t) {
+  printf("t-value::%d\n", ((Test*) t)->a);
+}
+
 int main(void) {
- return  EXIT_SUCCESS;
+  Test t[] = {{.a = 3}, {.a = 2}, {.a = 1}};
+
+  ArrList *l = init_list(3, compare_test);
+  add_value(l, &t);
+  add_value(l, &t[1]);
+  add_value(l, &t[2]);
+
+  qsort(l->values, l->len, sizeof(void*), compare_test);
+
+  walk(l, show_test);
+
+  return  EXIT_SUCCESS;
 }
