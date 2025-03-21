6ec6e1ed418016286c9563265a214d707bcc149e
mrl00 <m2l_0@outlook.com>
2025-03-21 17:48:39 -0300

    feat: add gitignore

--- graph.c
+++ graph.c
@@ -5,14 +5,28 @@ typedef struct {
   void *element;
 } vertex;
 
+typedef struct {
+  vertex *a, *b;
+} edge;
+
+typedef struct graph {
+  int (*match)(const edge *);
+} graph;
+
+vertex *new_vertex(const void *element);
+
+edge *new_edge(const vertex *, const vertex *);
+
+int main(int argc, char *argv[]) { return EXIT_SUCCESS; }
+
 vertex *new_vertex(const void *element) {
   vertex *v = (vertex *)calloc(1, sizeof(vertex));
   v->element = element;
   return v;
 }
 
-typedef struct {
-  vertex *a, *b;
-} edge;
-
-int main(int argc, char *argv[]) { return EXIT_SUCCESS; }
+edge *new_edge(const vertex *a, const vertex *b) {
+  edge *e = (edge *)calloc(1, sizeof(edge));
+  e->a = a, e->b = b;
+  return e;
+}
