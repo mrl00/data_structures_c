4e8db8ad9f4b1c23e8785a62d906c1412a4d2b5e
mrl00 <m2l_0@outlook.com>
2025-03-24 16:19:08 -0300

    feat: create set header and impl

---
+++ set.c
@@ -0,0 +1,70 @@
+#include "set.h"
+#include "list.h"
+#include <stdio.h>
+
+const char *set_status_string(enum SET_STATUS_ENUM code) {
+  switch (code) {
+  case SET_OK_ADDED_ELEMENT:
+    return "SET_OK_ADDED_ELEMENT";
+  case SET_ERROR_CANNOT_ADD_ELEMENT:
+    return "SET_ERROR_CANNOT_ADD_ELEMENT";
+  case SET_ERROR_CANNOT_ADD_ELEMENT_ALREADY_EXISTS:
+    return "SET_ERROR_CANNOT_ADD_ELEMENT_ALREADY_EXISTS";
+  case SET_ERROR_CANNOT_ADD_ELEMENT_NOT_ADJ:
+    return "SET_ERROR_CANNOT_ADD_ELEMENT_NOT_ADJ";
+  case SET_EMPTY:
+    return "SET_EMPTY";
+  }
+}
+
+bool set_is_empty(set *ss) { return list_is_empty(ss->l); }
+
+size_t set_size(set *ss) { return ss->l->length; }
+
+set *set_new(int (*compare)(const void *, const void *)) {
+  set *ss = (set *)calloc(1, sizeof(set));
+  ss->l = list_new(compare);
+  return ss;
+}
+
+bool set_is_member(set *ss, const void *value) {
+  if (list_find(ss->l, value) != NULL)
+    return true;
+  return false;
+}
+
+set_status set_add(set *ss, const void *value) {
+  if (set_is_member(ss, value))
+    return (set_status){.n = NULL,
+                        .status = SET_ERROR_CANNOT_ADD_ELEMENT_ALREADY_EXISTS};
+
+  list_status ls = list_add_last(ss->l, value);
+
+  switch (ls.status) {
+  case LIST_OK_ADDED_ELEMENT:
+    return (set_status){.n = ls.n, .status = SET_OK_ADDED_ELEMENT};
+  case LIST_ERROR_NODES_ARE_NOT_ADJACENTS:
+    return (set_status){.n = NULL,
+                        .status = SET_ERROR_CANNOT_ADD_ELEMENT_NOT_ADJ};
+  default:
+    return (set_status){.n = NULL, .status = SET_ERROR_CANNOT_ADD_ELEMENT};
+  }
+}
+
+int compare_int(const void *a, const void *b) { return *(int *)a - *(int *)b; }
+
+int main(void) {
+  int xs[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+  set *ss = set_new(compare_int);
+
+  set_status stt = set_add(ss, &xs[2]);
+  printf("%s\n", set_status_string(stt.status));
+
+  stt = set_add(ss, &xs[2]);
+  printf("%s\n", set_status_string(stt.status));
+
+  printf("%d\n", set_size(ss) == 1);
+
+  return EXIT_SUCCESS;
+}
---
+++ set.h
@@ -0,0 +1,33 @@
+#ifndef __SET_H__
+#define __SET_H__
+
+#include "list.h"
+#include <stdlib.h>
+
+enum SET_STATUS_ENUM {
+  SET_OK_ADDED_ELEMENT,
+  SET_ERROR_CANNOT_ADD_ELEMENT,
+  SET_ERROR_CANNOT_ADD_ELEMENT_ALREADY_EXISTS,
+  SET_ERROR_CANNOT_ADD_ELEMENT_NOT_ADJ,
+  SET_EMPTY,
+};
+
+typedef struct {
+  node *n;
+  enum SET_STATUS_ENUM status;
+} set_status;
+
+typedef struct __set {
+  list *l;
+} set;
+
+const char *set_status_string(enum SET_STATUS_ENUM);
+bool set_is_empty(set *);
+size_t set_size(set *);
+
+set *set_new(int (*compare)(const void *, const void *));
+
+bool set_is_member(set *, const void *value);
+set_status set_add(set *, const void *value);
+
+#endif
